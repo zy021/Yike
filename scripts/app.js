0364b70af14821e2b72c6ebb3d094001f00f8f1e
zy <zy12345@qq.com>
2017-11-30 15:07:21 +0800

    第二次提交
    导航栏修改

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 // 创建应用模块
-var app = angular.module("yike",[]);
+var app = angular.module("yike",["Ctrls"]);
 //run方法的作用是在模块创建好之后直接执行
 //因为toggle切换页面的方法对所有页面都有效,所以将该方法绑定在根作用域上
 app.run(["$rootScope",function($rootScope){
@@ -15,10 +15,19 @@ app.run(["$rootScope",function($rootScope){
 		//循环遍历每一个dd,修改dd的样式,达到显示隐藏的效果
 		if ($rootScope.collapsed) {
 			//如果这个值为true,表示显示
-			
+			//遍历每一个dd,显示
+			for(var i = 0;i < dds.length;i++){
+				//设置位置的移动
+				dds[i].style.transform = "translate(0)";
+				//时间差
+				dds[i].style.transitionDuration = (i+2)*0.15+"s";
+			}
 		}else{
 			//这个值为false,表示隐藏
-			
+			for(var i = 0;i < dds.length;i++){
+			dds[i].style.transform = "translate(-100%)";
+			dds[i].style.transitionDuration = (dds.length-i)*0.15+"s";
+			}
 		}
 	}
 }]);
---
+++ scripts/controllers.js
@@ -0,0 +1,15 @@
+//该文件用于存放控制器
+//创建一个独立的模块,该模块的作用就是创建不同的控制器,
+//方便其他模块之间使用
+angular.module("Ctrls",[])
+//创建控制器navs,模拟导航栏数据
+.controller("navs",["$scope",function($scope){
+	$scope.navs = [
+		{"href":"#/index","icon":"icon-home","text":"今日一刻"},
+		{"href":"#/older","icon":"icon-file-empty","text":"往期内容"},
+		{"href":"#/author","icon":"icon-pencil","text":"热门作者"},
+		{"href":"#/category","icon":"icon-menu","text":"栏目浏览"},
+		{"href":"#/favourite","icon":"icon-heart","text":"我的喜欢"},
+		{"href":"#/settings","icon":"icon-cog","text":"设置"}
+	];
+}])
